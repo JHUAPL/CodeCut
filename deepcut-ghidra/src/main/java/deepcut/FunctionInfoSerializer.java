ae7979a0138895f6cd8f88f38ad0e51494aea9bc
evm <evm.ftw@gmail.com>
2022-12-20 17:03:14 -0500

    Fixed extra directories

---
+++ deepcut-ghidra/src/main/java/deepcut/FunctionInfoSerializer.java
@@ -0,0 +1,47 @@
+/* ###
+* © 2022 The Johns Hopkins University Applied Physics Laboratory LLC
+* (JHU/APL).
+*
+* NO WARRANTY, NO LIABILITY. THIS MATERIAL IS PROVIDED “AS IS.” JHU/APL
+* MAKES NO REPRESENTATION OR WARRANTY WITH RESPECT TO THE PERFORMANCE OF
+* THE MATERIALS, INCLUDING THEIR SAFETY, EFFECTIVENESS, OR COMMERCIAL
+* VIABILITY, AND DISCLAIMS ALL WARRANTIES IN THE MATERIAL, WHETHER
+* EXPRESS OR IMPLIED, INCLUDING (BUT NOT LIMITED TO) ANY AND ALL IMPLIED
+* WARRANTIES OF PERFORMANCE, MERCHANTABILITY, FITNESS FOR A PARTICULAR
+* PURPOSE, AND NON-INFRINGEMENT OF INTELLECTUAL PROPERTY OR OTHER THIRD
+* PARTY RIGHTS. ANY USER OF THE MATERIAL ASSUMES THE ENTIRE RISK AND
+* LIABILITY FOR USING THE MATERIAL. IN NO EVENT SHALL JHU/APL BE LIABLE
+* TO ANY USER OF THE MATERIAL FOR ANY ACTUAL, INDIRECT, CONSEQUENTIAL,
+* SPECIAL OR OTHER DAMAGES ARISING FROM THE USE OF, OR INABILITY TO USE,
+* THE MATERIAL, INCLUDING, BUT NOT LIMITED TO, ANY DAMAGES FOR LOST
+* PROFITS.
+*
+* This material is based upon work supported by the Defense Advanced Research
+* Projects Agency (DARPA) and Naval Information Warfare Center Pacific (NIWC Pacific)
+* under Contract Number N66001-20-C-4024.
+*
+* HAVE A NICE DAY.
+*/
+
+package deepcut;
+
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+class FunctionInfoSerializer implements JsonSerializer<FunctionInfo> {
+	@Override
+	public JsonElement serialize(FunctionInfo src, Type typeOfSrc, JsonSerializationContext context) {
+		JsonObject obj = new JsonObject();
+		obj.addProperty("name", src.getName());
+		obj.addProperty("addr", src.getAddress().toString());
+		obj.addProperty("index", src.getAddressIndex());
+		obj.addProperty("num_incoming_edges", src.getIncomingEdges().size());
+		obj.addProperty("num_outgoing_edges", src.getOutgoingEdges().size());
+		
+		return obj;
+	}
+}
